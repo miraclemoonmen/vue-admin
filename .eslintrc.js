e70418b6980b2386ba2920924a2837de1387b167
goldcowboi <miraclecowboy@outlook.com>
2022-06-29 12:26:47 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     withDefaults: 'readonly'
   },
   rules: {
+    'vue/multi-word-component-names': ['error', { ignores: ['index'] }],
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     '@typescript-eslint/no-explicit-any': ['off']
